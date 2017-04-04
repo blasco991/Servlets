f639b35966fc4b3569b0fa2ce35f44f36e6029ac
blasco991 <blasco991@gmail.com>
2017-04-04 14:52:14 +0200

    Deploy

--- src/com/blasco991/chatServlet/LastIndex.java
+++ src/com/blasco991/chatServlet/LastIndex.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ListMessages
  */
-@WebServlet("/ListMessages")
+@WebServlet("/LastIndex")
 public class LastIndex extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
